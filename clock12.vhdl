7408fdc33770d40e98d62c69198338a9ee7a16b1
rogeliozazueta <61670163+rogeliozazueta@users.noreply.github.com>
2020-05-11 23:10:56 -0500

    Update clock12.vhdl

--- clock12.vhdl
+++ clock12.vhdl
@@ -42,16 +42,7 @@ end component;
     signal enableh: bit;
 begin
 
-   
-
-j3 <= qaux0 and qaux1 and qaux2;
-k3 <= qaux0;
-j2 <= qaux1 and qaux0;
-k2 <= qaux1 and qaux0;
-j1 <= qaux3n and qaux0;
-k1 <= qaux0;
-j0 <= '1';
-k0 <= '1';
+  
     
 jk11 : ffjk port map(clk,'1',j0,k0,qaux0,qaux0n);
 jk12: ffjk port map(clk,'1',j1,k1,qaux1,qaux1n);
@@ -69,10 +60,8 @@ mu(3) <= qaux3;
  decenasffk3 <= '1';
  decenasffj2 <= decenasq1 and decenasq;
  decenasffk2 <= decenasq;
- decenasffj1 <= decenasqn2 and decenasq;
  decenasffk1 <= decenasq;
- decenasffj0 <= '1';
- decenasffk0 <= '1';
+
 
  jk21 : ffjk port map(clk,enablem,decenasffj0,decenasffk0,decenasq,decenasqn0);
  jk22 : ffjk port map(clk,enablem,decenasffj1,decenasffk1,decenasq1,decenasqn1);
@@ -84,34 +73,4 @@ mu(3) <= qaux3;
  md(2) <= decenasq2;
  md(3) <= decenasq3;
 
- 
- enableh <= (qaux0 and qaux1n and qaux2n and qaux3 and decenasq and decenasqn1 and decenasq2 and decenasqn3);
- 
-
- jenable  <= horasq3 and horasq0;
- kenable  <= horasq1;
- hj3 <= horasq2 and horasq1 and horasq0;
- hk3 <= horasq0;
- hj2 <= horasq1 and horasq0;
- hk2 <= horasq1 and horasq0;
- hj1 <= horasqn3 and horasq0;
- hk1 <= qtemp or horasq0;
- hj0 <= '1';
- hk0 <= '1'; 
-
-
- jke :  ffjk port map(clk,enableh,jenable,kenable,qtemp,qtempnegada);
- jk0h : ffjk port map(clk,enableh,hj0,hk0,horasq0,horasqn0);
- jk1h : ffjk port map(clk,enableh,hj1,hk1,horasq1,horasqn1);
- jk2h : ffjk port map(clk,enableh,hj2,hk2,horasq2,horasqn2);
- jk3h : ffjk port map(clk,enableh,hj3,hk3,horasq3,horasqn3);
-
- hu(0) <= horasq0;
- hu(1) <= horasq1;
- hu(2) <= horasq2;
- hu(3) <= horasq3;
- hd <= qtemp;
-
-
-
-end behavioral;
+ X
